9ec91298fcecdfabe7fe9e77ebc5b62db1f28dd9
Leon Jamison <leonjamison@icloud.com>
2020-01-08 15:11:11 -0500

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,61 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :size, :color, :name, :brand, :material; :condition
+
+    def initialize(given_shoe)
+        @given_shoe = given_shoe
+    end 
+
+    def brand=(brand)
+        @brand = Nike
+    end 
+
+    def brand
+        @brand
+    end 
+
+    def name=(name)
+        @name = name
+    end 
+
+    def name
+        @name
+    end 
+
+    def size=(size)
+        @size = size
+    end
+
+    def size
+        @size
+    end 
+
+    def color=(color)
+        @color = color
+    end 
+
+    def color
+        @color
+    end 
+
+    def material=(material)
+        @material = material
+    end 
+
+    def material
+        @material
+    end 
+
+    def condition=(condition)
+        @condition = condition
+    end 
+
+    def condition
+        @condition
+    end
+
+    def repair
+        puts "That show has been repairs"
+    end 
+
+end 
